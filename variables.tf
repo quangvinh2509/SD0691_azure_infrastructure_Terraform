a28430ace4bb56cca9ec810e2f21b54981532bdd
quangvinh2509 <maiquangvinh2509@gmail.com>
2025-10-27 15:10:37 +0700

    Commit all code
    Commit all code

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  type        = string
+  description = "aks region where the resources are being created"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "aks cluster name, same name is used for resource group, vnet and subnets"
+  default     = "devop-aks"
+}
+
+variable "k8s_version" {
+  type        = string
+  description = "k8s version"
+  default     = "1.31"
+}
+
+variable "acr_name" {
+  type        = string
+  description = "Azure Container Registry name"
+  default     = "devopacr"
+}
